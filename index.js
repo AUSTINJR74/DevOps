923f725cde1f321e7d1248745cdc0b0b8aea99c9
Austin Jose R <austinjose.r@vakilsearch.com>
2026-01-17 12:29:44 +0530

    copy changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("App is live via Jenkins. 🚀");
+  res.send("App is live via Jenkins Auto Deployment. 🚀");
 });
 
 app.listen(3000, () => {
